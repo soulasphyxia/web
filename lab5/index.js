2dfe1e64f6a02412c1d304869b65d67f9555617c
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-03-12 18:04:23 +0700

    lab5 commit

--- lab5/index.js
+++ lab5/index.js
@@ -1,12 +1,36 @@
+function parseUserAgent(useragent) {
+    const platform = getPlatform(useragent);
+    const browser = getBrowser(useragent);
+    return {
+        "platform": platform,
+        "browser": browser
+    };
+}
+
+function getPlatform(useragent) {
+    getBrowser(useragent);
+    let platform = "Unknown";
+    if (useragent.indexOf("Win") != 1) platform = "Windows";
+    if (useragent.indexOf("Mac") != 1) platform = "MacOS";
+    if (useragent.indexOf("Linux") != 1) platform = "Linux";
+    return platform;
+}
+
+function getBrowser(useragent) {
+    const regexp = /(Chrome|Firefox|Egde|Safari|Opera)\/\d+/;
+    const browser_info = useragent.match(regexp)[0].split("/");
+    return {
+        "name": browser_info[0],
+        "version": browser_info[1]
+    }
+
+}
 const info = window.navigator;
+const user_agent = info.userAgent;
+const parsed = parseUserAgent(user_agent);
+
+document.getElementById("os-name").innerText = parsed.platform;
+document.getElementById("browser-name").innerText = parsed.browser.name;
+document.getElementById("browser-version").innerText = parsed.browser.version;
 
 
-console.log(info)
-
-const user_data = info.userAgentData
-
-const browser_info = user_data.brands[2]
-
-console.log(browser_info.brand)
-console.log(browser_info.version)
-console.log(user_data.platform)
