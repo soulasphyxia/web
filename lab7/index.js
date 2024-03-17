2172bc782fb35df76a996521cc658ff9cb20f03f
Vitaly Maslyuk <98162330+soulasphyxia@users.noreply.github.com>
2024-03-18 00:14:11 +0700

    lab 7 commit

--- lab7/index.js
+++ lab7/index.js
@@ -2,13 +2,10 @@ function isInternal(link) {
     if (link.startsWith("http://internal.com")) return false;
     return /:\/\//.test(link);
 }
-
 const links = document.getElementsByTagName("a");
-
 Array.from(links).forEach(link => {
-    const href = link.innerHTML
-    console.log(href)
-    if (isInternal(href)) {
+    const href = link.getAttribute("href")
+    if (href !== null && isInternal(href)) {
         link.style.color = "orange"
     }
 })
